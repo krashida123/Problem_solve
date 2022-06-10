1dc61e85383f6fd9ac5dcfffa7664c0dfe16c20e
kaniz <kaniz0808@gmail.com>
2022-06-10 18:57:13 -0400

    new class

---
+++ src/test/java/GroupPractice/Todays_Project/Class_today.java
@@ -0,0 +1,15 @@
+package GroupPractice.Todays_Project;
+
+public class Class_today {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String x = "Asma";
+		
+		
+		
+		
+	}
+
+}
